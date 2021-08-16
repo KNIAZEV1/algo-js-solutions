6a2db5b028c0d673587136174efb19779d9c1cbd
Ilia Kniazev <ilia.kniazev1@gmail.com>
2021-08-17 00:51:59 +0300

    refactoring sumZero

--- problems/multiple-pointers/first-pair-zero-sum/solution.js
+++ problems/multiple-pointers/first-pair-zero-sum/solution.js
@@ -9,14 +9,16 @@ export function sumZero(arr) {
 	let left = 0;
 	let right = arr.length - 1;
 
-	while (arr[left] < arr[right]) {
-		if (arr[left] + arr[right] === 0) {
+	while (left < right) {
+		let sum = arr[left] + arr[right]
+
+		if (sum === 0) {
 			return [arr[left], arr[right]];
 		}
-		if (arr[left] < arr[right]) {
-			left++;
-		} else {
+		if (sum > 0) {
 			right--;
+		} else {
+			left++;
 		}
 	}
 }
