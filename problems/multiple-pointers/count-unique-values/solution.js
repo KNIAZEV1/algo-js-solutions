94f77de1224260ef4ead80bc42f7edb91805410e
Ilia Kniazev <ilia.kniazev1@gmail.com>
2021-08-18 00:09:50 +0300

    refactored countUniqueValues solution

--- problems/multiple-pointers/count-unique-values/solution.js
+++ problems/multiple-pointers/count-unique-values/solution.js
@@ -3,10 +3,26 @@
  * @return {number}
  */
 
+// a soultion using Multiple pointers pattern
 export function countUniqueValues(arr) {
 	if (arr.length === 0) return 0;
 
-	const unique = new Set(arr)
-	
-	return unique.size
+	let i = 0;
+	for (let j = 1; j < arr.length; j++) {
+		if (arr[i] !== arr[j]) {
+			i++;
+			arr[i] = arr[j];
+		}
+	}
+
+	return i + 1;
+}
+
+// a short-hand solution using Set
+function countUniqueValues2(arr) {
+	if (arr.length === 0) return 0;
+
+	const unique = new Set(arr);
+
+	return unique.size;
 }
