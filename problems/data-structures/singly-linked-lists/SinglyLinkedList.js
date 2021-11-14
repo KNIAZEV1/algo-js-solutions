cd983ee0a420b0e91b01b62689093ea17923146d
ILIA <ilia.kniazev1@gmail.com>
2021-11-14 14:48:59 +0300

    added sll get

--- problems/data-structures/singly-linked-lists/SinglyLinkedList.js
+++ problems/data-structures/singly-linked-lists/SinglyLinkedList.js
@@ -1,87 +1,101 @@
 class Node {
 	constructor(val) {
-		this.val = val
-		this.next = null
+		this.val = val;
+		this.next = null;
 	}
 }
 
 class SinglyLinkedList {
 	constructor() {
-		this.head = null
-		this.tail = null
-		this.length = 0
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
 	}
 
 	push(val) {
-		const newNode = new Node(val)
-		
+		const newNode = new Node(val);
+
 		if (!this.head) {
-			this.head = newNode
-			this.tail = this.head
+			this.head = newNode;
+			this.tail = this.head;
 		} else {
-			this.tail.next = newNode
-			this.tail = newNode
+			this.tail.next = newNode;
+			this.tail = newNode;
 		}
 
-		this.length++
+		this.length++;
 
-		return this
+		return this;
 	}
 
 	pop() {
-		if (!this.head) return undefined
+		if (!this.head) return undefined;
 
-		let current = this.head
-		let newTail = current
+		let current = this.head;
+		let newTail = current;
 
-		while(current.next) {
-			newTail = current
-			current = current.next
+		while (current.next) {
+			newTail = current;
+			current = current.next;
 		}
 
-		this.tail = newTail
-		this.tail.next = null
-		this.length--
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
 
 		if (this.length === 0) {
-			this.head = null
-			this.tail = null
+			this.head = null;
+			this.tail = null;
 		}
 
-		return current
+		return current;
 	}
 
 	shift() {
-		if (!this.head) return undefined
+		if (!this.head) return undefined;
 
-		const currentHead = this.head
-		const newHead = currentHead.next
+		const currentHead = this.head;
+		const newHead = currentHead.next;
 
-		this.head = newHead
-		this.length--
+		this.head = newHead;
+		this.length--;
 
 		if (this.length === 0) {
-			this.tail = null
+			this.tail = null;
 		}
 
-		return currentHead
+		return currentHead;
 	}
 
 	unshift(val) {
-		const newNode = new Node(val)
+		const newNode = new Node(val);
 
 		if (!this.head) {
-			this.head = newNode
-			this.tail = this.head
+			this.head = newNode;
+			this.tail = this.head;
 		} else {
-			const currentHead = this.head
-			
-			newNode.next = currentHead
-			this.head = newNode
+			const currentHead = this.head;
+
+			newNode.next = currentHead;
+			this.head = newNode;
 		}
 
-		this.length++
+		this.length++;
 
-		return this
+		return this;
+	}
+
+	get(idx) {
+		if (idx < 0 || idx >= this.length) return undefined;
+
+		let current = this.head;
+		let count = 0;
+
+		while (count !== idx) {
+			current = current.next;
+			count++;
+		}
+
+		return current;
 	}
 }
