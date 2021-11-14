89864213947305bda036423dbb6ae6632892eed5
ILIA <ilia.kniazev1@gmail.com>
2021-11-14 15:55:58 +0300

    added sll reverse

--- problems/data-structures/singly-linked-lists/SinglyLinkedList.js
+++ problems/data-structures/singly-linked-lists/SinglyLinkedList.js
@@ -140,4 +140,23 @@ class SinglyLinkedList {
 
 		return nodeToRemove;
 	}
+
+	reverse() {
+		let node = this.head;
+
+		this.head = this.tail;
+		this.tail = node;
+
+		let next;
+		let prev = null;
+
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+
+		return this;
+	}
 }
