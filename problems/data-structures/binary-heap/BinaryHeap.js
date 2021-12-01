230b8ce78ac0395750d959fbc28c5500860d2bf5
ILIA <ilia.kniazev1@gmail.com>
2021-12-01 21:01:50 +0300

    added max bh insert

--- problems/data-structures/binary-heap/BinaryHeap.js
+++ problems/data-structures/binary-heap/BinaryHeap.js
@@ -1,3 +1,26 @@
-export class BinaryHeap {
-	constructor() {}
+export class MaxBinaryHeap {
+	constructor(list) {
+		this.values = list && list.length > 0 ? list : [];
+	}
+
+	insert(val) {
+		this.values.push(val);
+		this.bubbleUp();
+	}
+
+	bubbleUp() {
+		let index = this.values.length - 1;
+		const element = this.values[index];
+
+		while (index > 0) {
+			let parentIndex = Math.floor((index - 1) / 2);
+			const parent = this.values[parentIndex];
+
+			if (element <= parent) break;
+
+			this.values[parentIndex] = element;
+			this.values[index] = parent;
+			index = parentIndex;
+		}
+	}
 }
--- problems/data-structures/binary-heap/BinaryHeap.test.js
+++ problems/data-structures/binary-heap/BinaryHeap.test.js
@@ -1,7 +1,13 @@
-import { BinaryHeap } from './BinaryHeap';
+import { MaxBinaryHeap } from './BinaryHeap';
 
 describe('BinaryHeap', () => {
-	describe('', () => {
-		it('', () => {});
+	describe('MaxBinaryHeap', () => {
+		describe('insert', () => {
+			it('should return a valid list', () => {
+				const heap = new MaxBinaryHeap([41, 39, 33, 18, 27, 12]);
+				heap.insert(55);
+				expect(heap.values).toEqual([55, 39, 41, 18, 27, 12, 33]);
+			});
+		});
 	});
 });
