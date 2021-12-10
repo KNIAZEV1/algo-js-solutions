dbc053303811d20553414d25668d0e87bc67de25
ILIA <ilia.kniazev1@gmail.com>
2021-12-11 00:05:26 +0300

    added graph addVertex and addEdge

--- src/data-structures/graph/Graph.js
+++ src/data-structures/graph/Graph.js
@@ -3,4 +3,19 @@ export class Graph {
 	constructor() {
 		this.adjacencyList = {};
 	}
+
+	addVertex(vertex) {
+		if (!this.adjacencyList[vertex]) {
+			this.adjacencyList[vertex] = [];
+		}
+	}
+
+	addEdge(vertex1, vertex2) {
+		if (!this.adjacencyList[vertex1].includes(vertex2)) {
+			this.adjacencyList[vertex1].push(vertex2);
+		}
+		if (!this.adjacencyList[vertex2].includes(vertex1)) {
+			this.adjacencyList[vertex2].push(vertex1);
+		}
+	}
 }
--- src/data-structures/graph/Graph.test.js
+++ src/data-structures/graph/Graph.test.js
@@ -1,7 +1,40 @@
 import { Graph } from './Graph';
 
+function getGraphWithVertecies() {
+	const g = new Graph();
+
+	g.addVertex('Dallas');
+	g.addVertex('Florida');
+	g.addVertex('Miami');
+
+	return g;
+}
+
 describe('Graph', () => {
-	describe('', () => {
-		it('', () => {});
+	describe('addVertex', () => {
+		it('should return a valid adjacency list', () => {
+			const g = new Graph();
+
+			g.addVertex('Dallas');
+			expect(g.adjacencyList['Dallas']).toEqual([]);
+			g.addVertex('Tokyo');
+			expect(g.adjacencyList['Tokyo']).toEqual([]);
+		});
+	});
+	describe('addEdge', () => {
+		it('should return a valid adjacency list', () => {
+			const g = getGraphWithVertecies();
+
+			g.addEdge('Dallas', 'Miami');
+
+			expect(g.adjacencyList['Dallas']).toEqual(['Miami']);
+			expect(g.adjacencyList['Miami']).toEqual(['Dallas']);
+
+			g.addEdge('Dallas', 'Florida');
+
+			expect(g.adjacencyList['Dallas']).toEqual(['Miami', 'Florida']);
+			expect(g.adjacencyList['Florida']).toEqual(['Dallas']);
+			expect(g.adjacencyList['Miami']).toEqual(['Dallas']);
+		});
 	});
 });
