24873b32f3e11a0d1cbec078611f5ab0e1249885
ILIA <ilia.kniazev1@gmail.com>
2022-01-07 00:02:44 +0300

    added replaceElements in-place solution

--- src/problems/leetcode/multiple-pointers/1299-replace-with-greatest-on-rightside/solution.js
+++ src/problems/leetcode/multiple-pointers/1299-replace-with-greatest-on-rightside/solution.js
@@ -7,9 +7,24 @@ export function replaceElements(arr) {
 	const result = [];
 	result[arr.length - 1] = -1;
 
-	for (let i = arr.length - 1; i > 0; i -= 1) {
+	for (let i = arr.length - 1; i > 0; i--) {
 		result[i - 1] = Math.max(arr[i], result[i]);
 	}
 
 	return result;
+}
+
+// in-place solution, time: O(n), space: O(1)
+export function replaceElements2(arr) {
+	let max = arr[arr.length - 1];
+	arr[arr.length - 1] = -1;
+
+	for (let i = arr.length - 2; i >= 0; i--) {
+		let temp = arr[i];
+
+		arr[i] = max;
+		max = Math.max(max, temp);
+	}
+
+	return arr;
 }
--- src/problems/leetcode/multiple-pointers/1299-replace-with-greatest-on-rightside/solution.test.js
+++ src/problems/leetcode/multiple-pointers/1299-replace-with-greatest-on-rightside/solution.test.js
@@ -1,7 +1,8 @@
-import { replaceElements } from './solution';
+import { replaceElements, replaceElements2 } from './solution';
 
 describe('replaceElements', () => {
 	it('should replace every element in array with the greatest element among the elements to its right, and replace the last element with -1.', () => {
 		expect(replaceElements([17, 18, 5, 4, 6, 1])).toEqual([18, 6, 6, 6, 1, -1]);
+		expect(replaceElements2([17, 18, 5, 4, 6, 1])).toEqual([18, 6, 6, 6, 1, -1]);
 	});
 });
