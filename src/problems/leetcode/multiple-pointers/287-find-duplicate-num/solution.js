f3a9f723a23fbe5329700491722ed72ef9d770c5
ILIA <ilia.kniazev1@gmail.com>
2022-01-08 14:12:35 +0300

    improved findDuplicate

--- src/problems/leetcode/multiple-pointers/287-find-duplicate-num/solution.js
+++ src/problems/leetcode/multiple-pointers/287-find-duplicate-num/solution.js
@@ -7,14 +7,14 @@ export function findDuplicate(nums) {
 	let slow = nums[0];
 	let fast = nums[nums[0]];
 
-	while (slow != fast) {
+	while (slow !== fast) {
 		slow = nums[slow];
 		fast = nums[nums[fast]];
 	}
 
 	slow = 0;
 
-	while (slow != fast) {
+	while (slow !== fast) {
 		slow = nums[slow];
 		fast = nums[fast];
 	}
