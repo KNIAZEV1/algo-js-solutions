f80eef4b558ab9cfca473da333dc7961e5c3f5a8
ILIA <ilia.kniazev1@gmail.com>
2022-01-18 22:23:44 +0300

    added addTwoNumbers solution

---
+++ src/problems/leetcode/linked-lists/2-add-two-numbers/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+export function addTwoNumbers(l1, l2) {
+	return add(l1, l2, 0);
+
+	function add(l1, l2, carry) {
+		const value1 = l1?.val || 0;
+		const value2 = l2?.val || 0;
+		const sum = value1 + value2 + carry;
+		const newCarry = Math.floor(sum / 10);
+		const val = sum % 10;
+
+		return l1 || l2 || carry
+			? { val, next: add(l1?.next, l2?.next, newCarry) }
+			: null;
+	}
+}
---
+++ src/problems/leetcode/linked-lists/2-add-two-numbers/solution.test.js
@@ -0,0 +1,5 @@
+import { addTwoNumbers } from './solution';
+
+describe.skip('addTwoNumbers', () => {
+	it('should return the sum as a linked list.', () => {});
+});
